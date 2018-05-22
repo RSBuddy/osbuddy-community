e2bb0a7354c009625f54fe77a38a9cdd5928c5ba
matt <matt@automaton.uk>
2018-05-22 18:53:07 -0400

    Add emotes config

---
+++ api/src/main/java/com/rsbuddy/osrs/content/config/Emote.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2018 RSBuddy Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *    * Neither the name of RSBuddy Ltd. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.rsbuddy.osrs.content.config;
+
+public enum Emote {
+
+    YES,
+    NO,
+    BOW,
+    ANGRY,
+
+    THINK,
+    WAVE,
+    SHRUG,
+    CHEER,
+
+    BECKON,
+    LAUGH,
+    JUMP_FOR_JOY,
+    YAWN,
+
+    DANCE,
+    JIG,
+    SPIN,
+    HEADBANG,
+
+    CRY,
+    BLOW_KISS,
+    PANIC,
+    RASPBERRY,
+
+    CLAP,
+    SALUTE,
+    GOBLIN_BOW,
+    GOBLIN_SALUTE,
+
+    GLASS_BOX,
+    CLIMB_ROPE,
+    LEAN,
+    GLASS_WALL,
+
+    IDEA,
+    STAMP,
+    FLAP,
+    SLAP_HEAD,
+
+    ZOMBIE_WALK,
+    ZOMBIE_DANCE,
+    SCARED,
+    RABBIT_HOP,
+
+    SIT_UP,
+    PUSH_UP,
+    STAR_JUMP,
+    JOG,
+
+    ZOMBIE_HAND,
+    HYPERMOBILE_DRINKER,
+    SKILL_CAPE,
+    AIR_GUITAR,
+
+    URI_TRANSFORM,
+    SMOOTH_DANCE,
+    CRAZY_DANCE,
+    PREMIER_SHIELD,
+
+}
