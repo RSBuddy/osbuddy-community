2d52a6bb1673aa9fcd1b569677516dbc79b69823
James Thompson <james@automaton.uk>
2018-05-23 00:24:12 +0100

    Placeholder example

---
+++ plugins/example/src/main/java/com/rsbuddy/osrs/plugins/example/ExamplePlugin.java
@@ -0,0 +1,18 @@
+package com.rsbuddy.osrs.plugins.example;
+
+import com.rsbuddy.osrs.Plugin;
+import com.rsbuddy.osrs.PluginContext;
+
+public class ExamplePlugin implements Plugin {
+
+    @Override
+    public void init(PluginContext pluginContext) {
+
+    }
+
+    @Override
+    public void dispose(PluginContext pluginContext) {
+
+    }
+
+}
