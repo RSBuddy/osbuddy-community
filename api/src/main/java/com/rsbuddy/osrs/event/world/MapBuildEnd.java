e2e4a8d9b273bba0157981a6c61179623efcc17d
matt <matt@automaton.uk>
2018-10-08 16:47:32 -0400

    Create event for map build end

---
+++ api/src/main/java/com/rsbuddy/osrs/event/world/MapBuildEnd.java
@@ -0,0 +1,10 @@
+package com.rsbuddy.osrs.event.world;
+
+/**
+ * @author Matthew
+ */
+public class MapBuildEnd {
+
+    public static final MapBuildEnd MAP_BUILD_END_EVENT = new MapBuildEnd();
+
+}
